7ecef22b9c8e9a0b2628e05682f1c7052f015b4e
Jaylen Webb <jayrw6@gmail.com>
2024-10-28 18:12:22 -0500

    all buttons have functionality

--- main.js
+++ main.js
@@ -1,136 +1,206 @@
+const houses = ["Gryffindor", "Hufflepuff", "Slytherin", "Ravenclaw"];
+
 const students = [
 {
   id: 1,
   name: "Harry",
-  // imageURL: "https://assets.vogue.in/photos/5f23c04f1d33754d11eaf778/2:3/w_2240,c_limit/harry-potter-philosophers-stone-portrait-8.jpg",
-  house: "Gryffindor"
+  house: houses[0]
 },
 
 {
   id: 2,
   name: "Zacharias",
-  house: "Hufflepuff"
+  house: houses[1]
 },
 
 {
   id: 3,
   name: "Draco",
-  house: "Slytherin"
+  house: houses[2]
 },
 
 {
   id: 4,
   name: "Cho",
-  house: "Ravenclaw"
+  house: houses[3]
 },
 
 ];
 
+
 const badWiz = [
   {
-    id: students.length + 10,
+    id: "students",
     name: "Voldy",
     house: "Darkness"
   },
 ];
+// POPULATES FORM //
+beginButton.addEventListener("click", () => {
+  document.querySelector(".newStudent").style.display = "block";
+});
 
 
 
 
-
+// utility function //
 const renderToDom = (divId, htmlToRender) => {
   const targetingApp = document.querySelector(divId);
   targetingApp.innerHTML = htmlToRender;
   };
 
-  // Student cards to DOM //
 
-const cardsOnDom = (array) => {
+  // Student cards to DOM... tried .map funciton again but wouldn't work //
+
+// const cardsOnDom = (student) => {
+//   return `<div class="card mb-3" style="width:16rem;">
+//       <div class="card-body">
+//       <h3 class="card-title">${student.name}</h3>
+//       <p class="card-text">${student.house}</p>
+//       <a href="#" class="btn btn-danger" id="delete--${student.id}">Expel</a>
+//       </div>
+//     </div>`;
+// };
+// let domString = "";
+// students.map((student) => {
+//   domString += cardsOnDom(student);
+// });
+
+
+
+const cardsOnDom = (students) => {
   let domString = "";
-  students.forEach((student) => {
-    domString += `<div class="card mb-3" style="width:16rem;">
-      <div class="card-body">
-      <h3 class="card-title">${student.name}</h3>
-      <p class="card-text">${student.house}</p>
-      <a href="#" class="btn btn-danger" id="delete--${student.id}">Expel</a>
-      </div>
-    </div>`
-});
-
-renderToDom ("#app", domString);
+  for (const student of students) {
+    domString += `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+  <h3 class="card-title">${student.name}</h3>
+  <h5 class="card-title">${student.house}</h5>
+  <a href="#" class="btn btn-danger" id="delete--${student.id}">Expel</a>
+  </div>
+  </div>`;
 };
-
-const volOnDom = (array) => {
+renderToDom("#app", domString);
+};
+// Expelled student card //
+// const volOnDom = (badOne) => {
+//   return `<div class="card mb-3" style="width:16rem;">
+//       <div class="card-body">
+//       <h3 class="card-title">${badOne.name}</h3>
+//       <p class="card-text">${badOne.house}</p>
+//       </div>
+//     </div>`
+// };
+// let volDomString = ""
+// badWiz.map((badOne) => {
+//   volDomString += volOnDom (badOne);
+// });
+// renderToDom ("#volApp", volDomString);
+const volOnDom = (badWiz) => {
   let volDomString = "";
-  badOne.forEach((badOne) => {
-    volDomString += `<div class="card mb-3" style="width:16rem;">
-      <div class="card-body">
-      <h3 class="card-title">${badWiz.name}</h3>
-      <p class="card-text">${badWiz.house}</p>
-      </div>
-    </div>`
-});
-
-renderToDom ("#volApp", volDomString);
+  for (const badOne of badWiz) {
+    volDomString += `<div class="expelledCard" style="width: 18rem;">
+  <div class="card-body">
+  <h3 class="card-title">${badOne.name}</h3>
+  <h5 class="card-title">${badOne.house}</h5>
+  </div>
+  </div>`;
+}
+renderToDom("#volApp", volDomString)
 };
 
-// filter //
+
+
+
+
+
+
+
+
+
+
+// filter function //
 
 const filter = (array, houseString) => {
   const houseArray = [];
 
-  for (const student of array) {
+  array.forEach((student) => {
     if (student.house === houseString) {
       houseArray.push(student);
     }
-  }
+  });
   return houseArray;
 };
 
 
-// buttons //
-  const beginBtn = document.querySelector("#beginButton");
-  const sortBtn = document.querySelector("#houseBtn");
-  const enrolledStudents = document.querySelector("#enrollWiz");
   const form = document.querySelector("#wizardForm");
-  const expelledStudents = document.querySelector("#expelWiz");
+
+  // cardsOnDom(students);
+  // document.querySelector("#enrolled").style.display = "block";
+
+
+  // houseButton.addEventListener("click", (e) => {
+  //   e.preventDefault();
+
+    const addWizard = (e) => {
+      e.preventDefault();
+
+      const newWizard = {
+        id: students.length + 1,
+        name: document.querySelector("#nameHere").value,
+        house: houses[Math.floor(Math.random() * houses.length)],
+      };
+
+      students.push(newWizard);
+      cardsOnDom(students);
+      form.reset();
+    }
+
+    form.addEventListener("submit", addWizard);
+
+    const app = document.querySelector("#app");
+
+    app.addEventListener("click", (e) => {
+
+        if (e.target.id.includes("delete")) {
+        const [, id] = e.target.id.split("--");
+        const index = students.findIndex((e) => e.id === Number(id));
+        const removedStudent = students.splice(index, 1);
+        badWiz.push(removedStudent[0]);
+        cardsOnDom(students);
+        volOnDom(badWiz);
+      }
+    });
+
+  // document.querySelector("#app").style.display = "block";
+  const allBtn =document.querySelector("#allBtn");
   const gryffindor = document.querySelector("#gryffBtn");
   const hufflepuff = document.querySelector("#huffBtn");
   const ravenclaw = document.querySelector("#ravenBtn");
   const slytherin = document.querySelector("#slythBtn");
-  const app = document.querySelector ("#app");
-  const volApp = document.querySelector("#volApp");
 
-// event listeners //
-
-
-  beginButton.addEventListener ("click", () => {
-    cardsOnDom(students);
-  });
-    houseBtn.addEventListener ("click", () => {
-      cardsOnDom(students);
-  });
 
   allBtn.addEventListener("click", () => {
-  cardsOnDom(students);
-});
-  gryffindor.addEventListener("click", () => {
-    const gryffStudents = filter(students, "Gryffindor");
-    cardsOnDom(gryffStudents)
+    cardsOnDom(students);
   });
-  hufflepuff.addEventListener("click", () => {
-    const huffStudents = filter(students, "Hufflepuff");
-    cardsOnDom(huffStudents)
-  });
-  ravenclaw.addEventListener("click", () => {
-    const ravenStudents = filter(students, "Ravenclaw");
-    cardsOnDom(ravenStudents)
-   });
-  slytherin.addEventListener("click", () => {
-    const slythStudents = filter(students, "Slytherin");
-    cardsOnDom(slythStudents)
-  });
+    gryffindor.addEventListener("click", () => {
+      const gryffStudents = filter(students, "Gryffindor");
+      cardsOnDom(gryffStudents)
+    });
 
+    hufflepuff.addEventListener("click", () => {
+      const huffStudents = filter(students, "Hufflepuff");
+   cardsOnDom(huffStudents)
+    });
+
+    ravenclaw.addEventListener("click", () => {
+      const ravenStudents = filter(students, "Ravenclaw");
+     cardsOnDom(ravenStudents)
+     });
+
+    slytherin.addEventListener("click", () => {
+      const slythStudents = filter(students, "Slytherin");
+   cardsOnDom(slythStudents)
+    });
 
 
 
@@ -138,5 +208,4 @@ const filter = (array, houseString) => {
   const startApp = () => {
     cardsOnDom(students);
   };
-
-  startApp();
+startApp();
